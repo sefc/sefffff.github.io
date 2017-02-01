86590abdb9d4254c4c52c1e63c70cc1c1bdb0ac9
Sef <bleuounoir@gmail.com>
2017-01-31 21:19:18 -0800

    first upload

---
+++ js/contactus.js
@@ -0,0 +1,9 @@
+//nav//
+function openNav() {
+    document.getElementById("mySidenav").style.width = "300px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+//nav end//
